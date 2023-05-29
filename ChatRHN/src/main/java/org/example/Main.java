07d2d256007d3c6c810ba1895e5a559647fa8406
Raul Haro <raulharo99@gmail.com>
2023-05-29 01:05:49 -0400

    Added API calling function to the Java program

--- ChatRHN/src/main/java/org/example/Main.java
+++ ChatRHN/src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import org.example.service.ApiCallHandler;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    private Scanner scanner = new Scanner(System.in);
+    private ApiCallHandler caller = new ApiCallHandler();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main app = new Main();
+        app.run();
+    }
+
+    public void run() {
+        System.out.println("Please enter a prompt for the AI: ");
+        String userInput = scanner.nextLine();
+
+        String prompt = """
+                {
+                    "model": "gpt-3.5-turbo",
+                    "messages": [
+                        {
+                            "role": "system",
+                            "content": "You are sassy and give sassy answers to responses"
+                        },
+                        {
+                            "role": "user",
+                            "content": "%s"
+                        }
+                    ]
+                }
+                """.formatted(userInput);
+        try {
+            System.out.println("The AI's response: \n" + caller.promptAi(prompt));
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception thrown!");
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupted Exception thrown!");
+        }
     }
 }
--- ChatRHN/src/main/java/org/example/service/ApiCallHandler.java
+++ ChatRHN/src/main/java/org/example/service/ApiCallHandler.java
@@ -1,5 +1,50 @@
 package org.example.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import org.example.model.GptMessageModel;
+import java.net.http.HttpRequest;
+import java.util.Scanner;
+
 public class ApiCallHandler {
-    private final String OPENAI_URL = "https://api.openai.com/v1/completions"
+    private final String OPENAI_URL = "https://api.openai.com/v1/chat/completions";
+    private final String API_KEY = System.getenv("API_KEY");
+
+    // Need to implement this method to send the current conversation to the openai API and receive the response to
+    // display on the front end.
+    public String promptAi(String message) throws IOException, InterruptedException {
+        ObjectMapper mapper = new ObjectMapper();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(OPENAI_URL))
+                .header( "Content-Type", "application/json")
+                .header("Authorization", "Bearer " + API_KEY)
+                .POST(HttpRequest.BodyPublishers.ofString(message))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if(response.statusCode() == 200) {
+            String body = response.body();
+            JsonNode poopoocaca = mapper.readTree(body);
+            JsonNode scoop = poopoocaca.path("choices");
+
+            for (JsonNode node : scoop) {
+                JsonNode poop = node.path("message").path("content");
+                return poop.asText();
+            }
+        }
+
+        return "Cyrus is the key to the chain of apocalyptic events set to happen starting March 43rd 2043";
+    }
 }
