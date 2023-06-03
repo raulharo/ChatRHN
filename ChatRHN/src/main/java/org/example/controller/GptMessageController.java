f3ef69316be4a85a276f1f59bb24e638eae7b113
Raul Haro <raulharo99@gmail.com>
2023-06-03 01:29:02 -0400

    ITS ALIVER!!! Added conversation feature so responses from the user and the AI will be saved to a java class built to hold the conversation. Revamped the api call itself to use RestTemplate and HttpHeaders. Also made the mistake of using double quotes while the commit message contained exclamation marks. Never making that mistake again.

--- ChatRHN/src/main/java/org/example/controller/GptMessageController.java
+++ ChatRHN/src/main/java/org/example/controller/GptMessageController.java
@@ -2,5 +2,5 @@ package org.example.controller;
 
 public class GptMessageController {
     // Controller for when I, the incredibly handsome and extremely smart, Raul Haro, learn how to set up a
-    // tomcat server to handle http requests to the API endpoints I define here.
+    // tomcat server to handle http requests to the API endpoints I define here. Coming soon.
 }
