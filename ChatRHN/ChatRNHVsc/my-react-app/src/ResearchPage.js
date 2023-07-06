0b9c1e1875b1f0e7b9b8df5a323a68c29dd89eb7
Nicole <nguyenlnicole@gmail.com>
2023-07-06 17:18:43 -0500

    rawr

--- ChatRHN/ChatRNHVsc/my-react-app/src/ResearchPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/ResearchPage.js
@@ -8,7 +8,7 @@ function ResearchPage() {
       <h2>
         We learned most of our coding from Tech Elevator, and whatever we struggled with we used google.
         <br />
-        <h3>Go back to home <Link to="/">Home </Link></h3>
+        <h2><Link to="/">Go back to ChatRHN Home </Link></h2>
       </h2>
     </div>
   );
