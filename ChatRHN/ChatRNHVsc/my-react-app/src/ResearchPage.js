66b594819dc84417b48d7f2b7e23f7a341e0587d
Nicole <nguyenlnicole@gmail.com>
2023-07-06 17:23:15 -0500

    nicole

--- ChatRHN/ChatRNHVsc/my-react-app/src/ResearchPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/ResearchPage.js
@@ -8,6 +8,7 @@ function ResearchPage() {
       <h2>
         We learned most of our coding from Tech Elevator, and whatever we struggled with we used google.
         <br />
+        <br />
         <h2><Link to="/">Go back to ChatRHN Home </Link></h2>
       </h2>
     </div>
