ddf04a542907148087ab69c70bbf925dc5d0c868
Nicole <nguyenlnicole@gmail.com>
2023-07-06 17:28:03 -0500

    nicole

--- ChatRHN/ChatRNHVsc/my-react-app/src/ProductPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/ProductPage.js
@@ -6,7 +6,7 @@ function ProductsJustKidding() {
       <h1>Just Kidding!!</h1>
       <h2>We are not selling anything.</h2>
       <br />
-      < br />
+      <br />
       <h2><Link to="/">Go back to ChatRHN Home </Link></h2>
     </div>
   );
--- ChatRHN/ChatRNHVsc/my-react-app/src/SafetyPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/SafetyPage.js
@@ -6,7 +6,8 @@ function SafetyPage() {
   return (
     <div>
       <h1>If you do not like jokes </h1>
-      <h2>This is not for you. Do not</h2>
+      <h2>This is not for you. Do not</h2><br />
+      <br />
       <h2><Link to="/">Go back to ChatRHN Home </Link></h2>
     </div>
   );
