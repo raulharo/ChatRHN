fbfc6acdcc32740f4d208eea442dfe560a8588a7
Nicole <nguyenlnicole@gmail.com>
2023-07-06 17:28:58 -0500

    nicole

--- ChatRHN/ChatRNHVsc/my-react-app/src/CompanyPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/CompanyPage.js
@@ -6,6 +6,8 @@ function CompanyPage() {
     <div>
       <h1>Just Kidding!!!</h1>
       <h2>We're just 2 Jr. Developers having fun</h2>
+      <br />
+      <br />
       <h2><Link to="/">Go back to ChatRHN Home </Link></h2>
     </div>
   );
