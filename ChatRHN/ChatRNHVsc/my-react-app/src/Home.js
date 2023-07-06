063fd1c16a68cf924532c8f9b79e6077fe8e944d
Nicole <nguyenlnicole@gmail.com>
2023-07-06 17:39:36 -0500

    home

--- ChatRHN/ChatRNHVsc/my-react-app/src/Home.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/Home.js
@@ -1,43 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 import ChatGPTImage from './ChatGPT.png';
 import LogoChatRNHImage from './LogoChatRNH.png';
 
-function Home () {
-    return (
-        <>
-    <h1>Introducing ChatRHN</h1>
+function Home() {
+  return (
+    <>
+      <h1>Introducing ChatRHN</h1>
       <br />
 
       <h2>
-        We’ve trained a model called ChatRNH which interacts in an
+        We've trained a model called ChatRNH which interacts in an
         <br />
         awkward way. The dialogue format makes it difficult
         <br />
-        for ChatRHN to answer questions, will not admit its mistakes,
+        for ChatRHN to answer questions, admit its mistakes,
         <br />
-        and challenge premises, will give tons of sass, and may
+        challenge premises, and give tons of sass. It may
         <br />
-        
         or may not reject inappropriate requests.
-      </h2> 
+      </h2>
 
-       <div className="button-container">
+      <div className="button-container">
         <a href="ChatRNHSearch.html">
           <button>
             <b>Try ChatRHN</b>
           </button>
         </a>
         <p className="read-about">
-          <u>Read about ChatRNH (Just Kidding)</u>
+          <Link to="/">
+            <u>Read about ChatRNH (Just Kidding)</u>
+          </Link>
         </p>
       </div>
+
       <div className="chatGPT-image">
-      <img src={ChatGPTImage} alt="ChatGPT" style={{ maxWidth: '800px', marginLeft: '75px' }} />
+        <img src={ChatGPTImage} alt="ChatGPT" style={{ maxWidth: '800px', marginLeft: '75px' }} />
       </div>
 
       <img src={LogoChatRNHImage} alt="LogoChatRNH" />
-      
-      </>
-    )
+    </>
+  );
 }
 
 export default Home;
+
