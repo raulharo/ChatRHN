b70d7bb3b031cc25ee5fcad4d54eaa96ce619501
Nicole Nguyen <nguyenlnicole@gmail.com>
2023-07-13 18:59:16 -0500

    nicole

--- ChatRHN/ChatRNHVsc/my-react-app/src/CompanyPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/CompanyPage.js
@@ -8,7 +8,7 @@ function CompanyPage() {
       <h2>We're just 2 Jr. Developers having fun</h2>
       <br />
       <br />
-      <h2><Link to="/">Go back to ChatRHN Home </Link></h2>
+      <h2 ><Link to="/" style={{ color: '#FF8BFF' }}>Go back to ChatRHN Home </Link></h2>
     </div>
   );
 }
