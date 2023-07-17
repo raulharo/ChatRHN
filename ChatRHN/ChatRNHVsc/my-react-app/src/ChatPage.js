e2d83e709fd6c22f5607a6a87da3970576f89ab4
Raul Haro <raulharo99@gmail.com>
2023-07-16 23:22:57 -0400

    Switched ChatPage over to a class component and am directly altering the state. I know it isnt really best practice to do that but after a while it was really tough finding ways to make the useState hook work the way I needed it to.

--- ChatRHN/ChatRNHVsc/my-react-app/src/ChatPage.js
+++ ChatRHN/ChatRNHVsc/my-react-app/src/ChatPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import SearchBar from './SearchBar';
 import MessageBar from './MessageBar';
 import './ChatPage.css';
@@ -10,87 +10,75 @@ import axios from 'axios';
 // the API call to gpt-3.5-turbo. We need to figure out a way to make sure that the state is updated BEFORE the promptAi()
 // function is called or find a synchronous way to modify data.
 
-function ChatPage() {
+class ChatPage extends React.Component {
   // This data property using the useState hook is holding the response body that will be sent to our api with every post request
-  const [conversation, setConversation] = useState({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are sassy and give a sarcastic spin to all your replies, and most likely you do not know the answers."
-      }]
-  });
-  // This data property is holding the value of the text box where the user will input their prompt for the AI
-  const [userInput, setUserInput] = useState('');
-
-  // Supposedly updates the state when there is a change made to it, but doesn't seem to work
-  useEffect(() => {
-    console.log(conversation.messages);
-  }, [conversation.messages]);
-
-  // This function simply assigns the current value in the text box to the data prop above
-  const handleUserInput = (input) => {
-    setUserInput(input);
+  constructor(props) {
+    super(props);
+    this.state = {
+      conversation: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are sassy and give a sarcastic spin to all your replies, and most likely you do not know the answers."
+          }]
+      },
+      prompt: { 
+        userInput: "" 
+      }
+    }
   }
 
-  // Function to add the AIs response to the messages array
-  const addAiResponse = (response) => {
-    setConversation(conversation => ({
-      ...conversation,
-      messages: [...conversation.messages, response] 
-    }));
+  handleUserInput = (value) => {
+    this.state.prompt.userInput = value;
   }
 
-  // Adds the user's input into the messages array
-  const addUserInput = () => {
-    let newMessage = {role: "user", content: userInput};
+  addUserInput = () => {
+    let tempArray = this.state.conversation.messages;
+    let newMessage = {role: "user", content: this.state.prompt.userInput};
 
-    setConversation(conversation => ({
-      ...conversation,
-      messages: [...conversation.messages, newMessage]
-    }));
+    tempArray.push(newMessage);
+
+    this.state.conversation.messages = tempArray;
   }
 
-  // Just shows what the current array looks like in the console
-  const showArray = () => {
-    console.log(conversation.messages);
+  printConversation = () => {
+    console.log(this.state.conversation.messages);
   }
 
-  // This function will take the current conversation state and send it to the Java backend which
-  // will make an API call to the OpenAI API
-  const promptAi = async () => {
+  submitPrompt = () => {
+    this.addUserInput();
+
     try {
-      const response = await axios.post('http://localhost:8080/send-message', conversation);
-      return response.data;
+      axios.post('http://localhost:8080/send-message', this.state.conversation).then(response => {
+        let tempArray = this.state.conversation.messages;
+
+        tempArray.push(response.data);
+
+        this.state.conversation.messages = tempArray;
+      });
     }
     catch (error) {
       console.log(error);
     }
   }
 
-  // Attempt to see if calling addUserInput and promptAi separately would let the state update before
-  // the promptAi method is called
-  function runPrompt() {
-    addUserInput();
+  render() {
 
-    let aiMessage = promptAi();
-
-    addAiResponse(aiMessage);
-  }
-
-  return (
+    return (
     <div className="chat-page-container">
       <div className="message-bar-container">
-        <MessageBar setMessage={handleUserInput} />
-        <button onClick={runPrompt}>Submit</button>
-        <button onClick={showArray}>Messages Array</button>
+        <MessageBar setMessage={this.handleUserInput} />
+        <button onClick={this.submitPrompt}>Submit</button>
+        <button onClick={this.printConversation}>Messages</button>
       </div>
       <br />
       <div className="search-bar-container">
         <SearchBar />
       </div>
     </div>
-  );
+    );
+  }
 }
 
 export default ChatPage;
